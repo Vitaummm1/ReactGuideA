d890a6d5b4ecea2fba1cd6622c11083493a5c76a
Vitaummm1 <keyke@hotmail.com.br>
2022-03-09 17:05:35 -0300

    expense things and ui card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import ExpensesDisplay from './components/Expenses/ExpensesDisplay';
+
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Car Insurance',
+      price: Math.floor(Math.random() * 200),
+      date: new Date(2021, 2, 28)
+    },
+    {
+      id: 'e2',
+      title: 'Bills',
+      price: Math.floor(Math.random() * 200),
+      date: new Date(2021, 2, 28)
+    },
+    {
+      id: 'e3',
+      title: 'Market',
+      price: Math.floor(Math.random() * 200),
+      date: new Date(2021, 2, 28)
+    }
+  ]
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpensesDisplay
+      data={expenses}
+      />
     </div>
   );
 }
---
+++ src/components/Expenses/ExpensesDisplay.js
@@ -0,0 +1,27 @@
+import './ExpensesDisplay.css';
+import Card from '../UI/Card';
+import ExpenseItem from './ExpenseItem';
+
+function ExpensesDisplay(props){
+    return(
+    <Card className='expenses'>
+      <ExpenseItem 
+      title={props.data[0].title}
+      price={props.data[0].price}
+      date={props.data[0].date}
+      />
+      <ExpenseItem 
+      title={props.data[1].title}
+      price={props.data[1].price}
+      date={props.data[1].date}
+      />
+      <ExpenseItem 
+      title={props.data[2].title}
+      price={props.data[2].price}
+      date={props.data[2].date}
+      />
+    </Card>
+    )
+}
+
+export default ExpensesDisplay;
